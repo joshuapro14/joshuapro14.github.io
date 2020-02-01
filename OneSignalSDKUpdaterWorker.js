e7666555b0a55e14e181f7b6ebd2b71a13c4f0c0
unknown <joshuapro14@gmail.com>
2020-02-01 09:56:23 +0530

    updated service worker js file with some comments

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,1 +1,4 @@
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+
+//#1
+//from https://github.com/joshuapro14/joshuapro14.github.io.git
--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,4 @@
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+
+//#2
+//from https://github.com/joshuapro14/joshuapro14.github.io.git
